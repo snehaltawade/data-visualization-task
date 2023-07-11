a75a043f89c1ad73c6f700489a00f1e52d46abcf
Deepak Tawade, Snehal <snehal.deepak.tawade@accenture.com>
2023-07-11 18:07:36 +0530

    utils format updated

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,19 +1,17 @@
-export const mean =(arrayOfNumbers)=>{
+export const mean = (arrayOfNumbers) => {
     let sum = 0;
-    let arrayLen=arrayOfNumbers.length
+    let arrayLen = arrayOfNumbers.length
     for (let i = 0; i < arrayLen; i++)
         sum += Number(arrayOfNumbers[i]);
     return sum / arrayLen;
 }
 
-export const mode =(arrayOfNumbers)=>{
-    let mode=0,max=0,k=0,c=0,arrayLen=arrayOfNumbers.length;
-    let modeArray=[]
-    for (var i = 0; i < arrayLen - 1; i++)
-    {
+export const mode = (arrayOfNumbers) => {
+    let mode = 0, max = 0, k = 0, total = 0, arrayLen = arrayOfNumbers.length;
+    let modeArray = []
+    for (var i = 0; i < arrayLen - 1; i++) {
         mode = 0;
-        for (let j = i + 1; j < arrayLen; j++)
-        {
+        for (let j = i + 1; j < arrayLen; j++) {
             if (arrayOfNumbers[i] == arrayOfNumbers[j]) {
                 mode++;
             }
@@ -30,39 +28,34 @@ export const mode =(arrayOfNumbers)=>{
         }
     }
 
-    for (var i = 0; i < arrayLen; i++)
-    {
-        if (arrayOfNumbers[i] == modeArray[i]) 
-            c++;
+    for (var i = 0; i < arrayLen; i++) {
+        if (arrayOfNumbers[i] == modeArray[i])
+            total++;
     }
-    if (c == arrayLen)
-       return ("There is no mode");
-    else
-    {
+    if (total == arrayLen)
+        return ("no mode");
+    else {
         return modeArray;
     }
 }
-export const median =(a)=>{
-    let sortedArray=sort(a,a.length)
-    let n=a.length
-    if (n % 2 != 0){
-        return Number(sortedArray[(n+1)/2]);
+export const median = (a) => {
+    let sortedArray = sort(a, a.length)
+    let n = a.length
+    if (n % 2 != 0) {
+        return Number(sortedArray[(n + 1) / 2]);
     }
-        
- 
-    return (sortedArray[Math.floor((n-1)/2)] +sortedArray[n / 2]) / 2;
+
+
+    return (sortedArray[Math.floor((n - 1) / 2)] + sortedArray[n / 2]) / 2;
 }
 
-export const sort =(arr,n)=>{
+export const sort = (arr, n) => {
     var i, j, temp;
     var swapped;
-    for (i = 0; i < n - 1; i++) 
-    {
+    for (i = 0; i < n - 1; i++) {
         swapped = false;
-        for (j = 0; j < n - i - 1; j++) 
-        {
-            if (arr[j] > arr[j + 1]) 
-            {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -70,55 +63,45 @@ export const sort =(arr,n)=>{
             }
         }
         if (swapped == false)
-        break;
+            break;
     }
     return arr;
 }
 
-export const classBasedFilter =(myData)=>{
+export const classBasedFilter = (myData) => {
 
     const filteredArray = myData.reduce((classBasedArray, data) => {
-      let {Alcohol}=data
-      if (!classBasedArray[Alcohol]) {
-        classBasedArray[Alcohol] = [];
-        classBasedArray[Alcohol].push(data);
-      }
-      else{
-        classBasedArray[Alcohol].push(data);
-      }
-      return classBasedArray;
+        let { Alcohol } = data
+        if (!classBasedArray[Alcohol]) {
+            classBasedArray[Alcohol] = [];
+            classBasedArray[Alcohol].push(data);
+        }
+        else {
+            classBasedArray[Alcohol].push(data);
+        }
+        return classBasedArray;
     }, []);
     return filteredArray;
-  }
+}
 
-export const filterProperty=(data,property)=>{
-    let result=[]
+export const filterProperty = (data, property) => {
+    let result = []
     data.forEach(element => {
-        // console.log(element[property])
         result.push(element[property])
     });
     return result
 }
 
-const parseStringNumbers=(array)=>{
+const parseStringNumbers = (array) => {
     var numberArray = [];
-    for (var i = 0; i < array.length; i++)
-       { numberArray.push(parseInt(array[i]))}
+    for (var i = 0; i < array.length; i++) { numberArray.push(parseInt(array[i])) }
     return numberArray
 }
 
-export const calculateGamma=(data)=>{
-    let result=[]
+export const calculateGamma = (data) => {
+    let result = []
     data.forEach(element => {
-        // console.log(element[property])
-        result.push(element['Ash']*element['Hue']/element['Magnesium'])
+        result.push(element['Ash'] * element['Hue'] / element['Magnesium'])
     });
-    console.log(result)
     return result
-}
-
-export const formatArray=(array)=>{
-    array.forEach(element=>{
-        return element+" ";
-    })
 }
